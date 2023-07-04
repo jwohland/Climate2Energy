20cd4862ed8b165d63a0e08095d9b708f5242a00
jwohland <jan.wohland@env.ethz.ch>
2023-07-04 11:59:53 +0200

    Country averaging included

--- code/country_average.py
+++ code/country_average.py
@@ -1,5 +1,78 @@
 import salem
+import xarray as xr
 
-shape_path = "../inputs/"
-land = salem.read_shapefile(shape_path + "ne_110m_land.shp", cached=True)
-mask = ds.salem.roi(shape=land, all_touched=True)
+
+def get_country_list():
+    """
+    List of countries that are included in energy models from
+    Leonard Goeke and Jonas Savelsberg
+    :return:
+    """
+    countries = [
+        "Albania",
+        "Austria",
+        "Bosnia and Herzegovina",
+        "Belgium",
+        "Bulgaria",
+        "Switzerland",
+        "Czech Republic",
+        "Germany",
+        "Denmark",
+        "Estonia",
+        "Spain",
+        "Finland",
+        "France",
+        "Greece",
+        "Croatia",
+        "Hungary",
+        "Ireland",
+        "Italy",
+        "Lithuania",
+        "Latvia",
+        "Montenegro",
+        "Macedonia",
+        "Netherlands",
+        "Norway",
+        "Poland",
+        "Portugal",
+        "Romania",
+        "Serbia",
+        "Sweden",
+        "Slovenia",
+        "Slovakia",
+        "United Kingdom",
+    ]
+    return countries
+
+
+def cut_out_countries(ds):
+    """
+    Split global input dataset per country.
+    #todo add similar computation for offshore wind
+    :param ds: dataset with new dimension country
+    :return:
+    """
+    shdf = salem.read_shapefile(salem.get_demo_file("world_borders.shp"))
+    ds_list = []
+    for country in get_country_list():
+        print(country)
+        shdf_tmp = shdf.loc[shdf["CNTRY_NAME"] == country]
+        ds_country = ds.salem.roi(shape=shdf_tmp, all_touched=True)
+        ds_country["country"] = country
+        ds_list.append(ds_country)
+    return xr.concat(ds_list, dim="country")
+
+def country_means(ds, method="all"):
+    """
+    Aggregate over a country
+
+    Method all takes an unweighted mean over all boxes within a country
+
+    Todo: Add more sophisticated averaging methods
+    :param ds:
+    :return:
+    """
+    ds = cut_out_countries(ds)
+    if method ==  "all":
+        ds = ds.mean(dim=["lat", "lon"])
+    return ds
