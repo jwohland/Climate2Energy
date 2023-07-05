78654201ff10f254841e8b9126d46ffd0e5fda5e
jwohland <jan.wohland@env.ethz.ch>
2023-07-05 14:35:25 +0200

    Full wind power CF conversion for a small dataset

--- code/conversion_to_CF.py
+++ code/conversion_to_CF.py
@@ -1,5 +1,9 @@
 from windpowerlib import wind_turbine as wt
 import numpy as np
+import pandas as pd
+import glob
+import time
+import xarray as xr
 
 
 out_path = "../output/"
@@ -35,4 +39,81 @@ def compute_powercurves():
         # save
         power_curve.to_pickle(out_path + 'final_power_curve_' + str(i) + '.p')
 
-1
+class Power:
+    """
+    Wind power  conversion class. Based on pre-computed power curves, this
+    class provides functionality to translate hub height wind speeds into
+    capacity factors.
+    """
+    def __init__(self, turbine_index):
+        self.power_curve = pd.read_pickle(sorted(glob.glob('../output/*.p'))[turbine_index])
+        self.turbine_name = self.power_curve.keys()[0].replace('/', '_')  # / leads to issues when saving
+
+    def power_conversion(self, s):
+        """
+        translate wind speed s into capacity factors via the power curves that
+        are provided as a lookup table.
+        :param s:
+        :return:
+        """
+        s = np.round(s, 2)  # only two decimal accuracy in power curve
+        if s < self.power_curve.index[0] or s > self.power_curve.index[-1]:
+            # below cut_in or above cut_out
+            out = 0.
+        else:
+            out = self.power_curve.loc[s].values[0]
+        return out
+
+
+def update_attrs(ds, var, unitname, varname, long_varname):
+    """
+    Updates metadata of an xarray dataset, for example after trend calculation
+    :param ds: input dataset
+    :param var; variable in ds that is to be updated
+    :param unitname: new units
+    :param varname: new name of the variable
+    :param long_varname: new long name of the variable
+    :return:
+    """
+    ds = ds.rename_vars({var: varname})
+    ds[varname].attrs['units'] = unitname
+    ds[varname].attrs['long_name'] = long_varname
+    return ds
+
+def convert_winds(ds, filename):
+    """
+    Convert wind speeds to wind capacity factors for the three turbines
+    :param ds: xr.dataset with hub height wind speeds availablee as "s_hub"
+    :param filename:
+    :return:
+    """
+    for turbine_index in range(3):
+        # Open power curves if they exists, otherwise compute them
+        try:
+            P = Power(turbine_index)
+        except:
+            compute_powercurves()
+            P = Power(turbine_index)
+        print(P.turbine_name)
+
+        #Check if this particular output already exists, otherwise compute
+        try:
+            xr.open_dataset(out_path + P.turbine_name + '/' + filename)
+            print(" already exists")
+        except FileNotFoundError:
+            t_0 = time.time()
+            wind_power = xr.apply_ufunc(P.power_conversion,
+                                        ds["s_hub"],
+                                        vectorize=True,
+                                        dask='allowed').to_dataset()
+            wind_power = update_attrs(wind_power,
+                                      's_hub',
+                                      '',
+                                      'CF_wind',
+                                      'normalized_wind_power_generation')
+            print('wind power conversion took ' + str(time.time() - t_0))
+            t_0 = time.time()
+            wind_power.to_netcdf(out_path + P.turbine_name + '/' + filename)
+            print('saving took ' + str(int(time.time() - t_0)) + ' s')
+
+
--- code/run_all.py
+++ code/run_all.py
@@ -4,16 +4,28 @@ from conversion_to_CF import *
 from country_average import *
 from utils import *
 
-data_path =  "/net/xenon/climphys/lbloin/energy_boost/"
+data_path = "/net/xenon/climphys/lbloin/energy_boost/"
 ds = xr.open_dataset(data_path + "CESM2_r1i1p1_2015_6h.nc")
 ds = zero_mean_longitudes(ds)
+
+# wind example over limited domain and just claiming that one level is hub height
+# todo this is just temporary
+ds_wind = (
+    ds.isel(lev=31, ilev=31)["U"]
+    .squeeze()
+    .drop(["lev", "ilev"])
+    .sel(lat=slice(50, 60), lon=slice(10, 20))
+)
+ds_wind = ds_wind.to_dataset(name="s_hub")
 
 # Step 1: Bias correction
 ds = bias_correct(ds)
 
 # Step 2: Calculate capacity factors
 ds_CF_PV = calculate_PV(ds)
-ds_CF_wind = calculate_wind(ds)
+ds_CF_wind = convert_winds(
+    ds_wind, "Wind_power_2015.nc"
+)  # this expects that ds has variable called s_hub with hub height winds
 
 # Step 3: subset countries
 df_PV = cut_out_countries(ds_CF_PV)
