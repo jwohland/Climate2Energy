7068d15bf2fc5be086e97bfd2b64bb03f16a71a1
jwohland <jan.wohland@env.ethz.ch>
2023-07-07 14:31:29 +0200

    execute PV computation with example data in run_all

--- code/run_all.py
+++ code/run_all.py
@@ -4,12 +4,13 @@ from conversion_to_CF import *
 from country_average import *
 from utils import *
 
+
 data_path = "/net/xenon/climphys/lbloin/energy_boost/"
 ds = xr.open_dataset(data_path + "CESM2_r1i1p1_2015_6h.nc")
 ds = zero_mean_longitudes(ds)
 
 # wind example over limited domain and just claiming that one level is hub height
-# todo this is just temporary
+# todo this is just temporary and needs to be generalized to full domain and correct heights
 ds_wind = (
     ds.isel(lev=31, ilev=31)["U"]
     .squeeze()
@@ -18,11 +19,25 @@ ds_wind = (
 )
 ds_wind = ds_wind.to_dataset(name="s_hub")
 
+# radiation example over limited domain and just claiming that one level is hub height
+# todo this is just temporary and needs to be expanded to full domain
+# gsee expects dataset with variables "global_horizontal" and "temperature"
+ds_PV = xr.open_dataset(data_path + "CESM2_r1i1p1_2015_daily_h2.nc")
+ds_PV = (
+    ds_PV["FSDS"].sel(lat=slice(50, 60), lon=slice(10, 20))
+)
+ds_PV = ds_PV.to_dataset(name="global_horizontal")
+ds_temp = xr.open_dataset(data_path + "CESM2_r1i1p1_2015_daily_h1.nc")
+ds_PV["temperature"] = ds_temp["TREFHT"]
+ds_PV["time"] = ds_PV.indexes["time"].to_datetimeindex()  # time index that GSEE understands
+
+
+
 # Step 1: Bias correction
 ds = bias_correct(ds)
 
 # Step 2: Calculate capacity factors
-ds_CF_PV = calculate_PV(ds)
+ds_CF_PV = calculate_PV(ds_PV, params=None)
 ds_CF_wind = convert_winds(
     ds_wind, "Wind_power_2015.nc"
 )  # this expects that ds has variable called s_hub with hub height winds
