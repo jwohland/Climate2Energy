1cbf401c944cbf6a0a278a341d8fbe43cd8ab2dd
jwohland <jan.wohland@env.ethz.ch>
2023-07-10 17:57:44 +0200

    Run over full temporal scope and also save outputs

--- code/run_all.py
+++ code/run_all.py
@@ -8,23 +8,17 @@ from utils import *
 data_path = "/net/xenon/climphys/lbloin/energy_boost/"
 ds = xr.open_dataset(data_path + "CESM2_r1i1p1_2015_6h.nc")
 
-# wind example over limited domain and just claiming that one level is hub height
-# todo this is just temporary and needs to be generalized to full domain and correct heights
+# wind example claiming that one level is hub height
 ds_wind = (
-    ds.isel(lev=31, ilev=31)["U"]
-    .squeeze()
-    .drop(["lev", "ilev"])
-    .isel(time=slice(0, 3))
-    .sel(lat=slice(35, 75), lon=slice(-12, 40))
+    ds.isel(lev=31, ilev=31)["U"].squeeze().drop(["lev", "ilev"]).sel(lat=slice(35, 75))
 )
-ds_wind = zero_mean_longitudes(ds_wind)
+ds_wind = zero_mean_longitudes(ds_wind).sel(lon=slice(-12, 40))
 ds_wind = ds_wind.to_dataset(name="s_hub")
 
-# radiation example over limited domain and just claiming that one level is hub height
-# todo this is just temporary and needs to be expanded to full domain
+# radiation example
 # gsee expects dataset with variables "global_horizontal" and "temperature"
 ds_PV = xr.open_dataset(data_path + "CESM2_r1i1p1_2015_daily_h2.nc")
-ds_PV = ds_PV["FSDS"].isel(time=slice(0, 3)).sel(lat=slice(35, 75), lon=slice(-12, 40))
+ds_PV = ds_PV["FSDS"].sel(lat=slice(35, 75), lon=slice(-12, 40))
 ds_PV = ds_PV.to_dataset(name="global_horizontal")
 ds_temp = xr.open_dataset(data_path + "CESM2_r1i1p1_2015_daily_h1.nc")
 ds_PV["temperature"] = ds_temp["TREFHT"]
@@ -45,3 +39,12 @@ ds_CF_wind = convert_winds(
 # Step 3: subset countries
 df_PV = cut_out_countries(ds_CF_PV)
 df_wind = cut_out_countries(ds_CF_wind)
+
+# Step4: Save data
+# wind
+for i in range(3):
+    ds = ds_wind.isel(turbine=i)
+    turbine_name = str(ds.turbine.values)
+    store_as_pandas_dataframe(ds["CF_wind"], name="CF_" + turbine_name)
+# PV
+store_as_pandas_dataframe(ds_PV["pv"], name="CF_PV")
