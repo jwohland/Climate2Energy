53eeeaf3684a73cd8a815d8ff058c52522013d33
Luna Bloin-Wibe <luna.bloinwibe@env.ethz.ch>
2023-10-04 14:42:25 +0200

    changed temporary file location to relative paths

--- code/preprocess/preprocess_global_horizontal_ERA5.sh
+++ code/preprocess/preprocess_global_horizontal_ERA5.sh
@@ -1,10 +1,10 @@
 for i in {1981..2010}
     do
     # remap (conservative) to CESM grid, select Europe, change name to temperature, convert to W/m2 (with units)
-    cdo -b F64 -divc,3600 -setattribute,global_horizontal@units="W/m^2" -dayavg -chname,ssrd,global_horizontal -sellonlatbox,-15,50,30,75 -remapcon,/net/meso/climphys/flehner/observations/era5/day/CESM_atm_grid.txt /net/atmos/data/ERA5_deterministic/recent/0.25deg_lat-lon_1h/processed/regrid/era5_deterministic_recent.ssrd.025deg.1h."${i}".nc /net/xenon/climphys/lbloin/energy_boost/tmp_"${i}"_global_horizontal.nc
+    cdo -b F64 -divc,3600 -setattribute,global_horizontal@units="W/m^2" -dayavg -chname,ssrd,global_horizontal -sellonlatbox,-15,50,30,75 -remapcon,/net/meso/climphys/flehner/observations/era5/day/CESM_atm_grid.txt /net/atmos/data/ERA5_deterministic/recent/0.25deg_lat-lon_1h/processed/regrid/era5_deterministic_recent.ssrd.025deg.1h."${i}".nc ../../output/tmp_"${i}"_global_horizontal.nc
 
 done
 
-cdo mergetime /net/xenon/climphys/lbloin/energy_boost/tmp_*_global_horizontal.nc ../../output/global_horizontal_ERA5.nc
+cdo mergetime ../../output/tmp_*_global_horizontal.nc ../../output/global_horizontal_ERA5.nc
 
-rm /net/xenon/climphys/lbloin/energy_boost/tmp_*_global_horizontal.nc
+rm ../../output/tmp_*_global_horizontal.nc
--- code/preprocess/preprocess_s_hub_model_hist.sh
+++ code/preprocess/preprocess_s_hub_model_hist.sh
@@ -2,12 +2,12 @@
 for year in {1981..2010}
     do
     # select lowest level (32 because cdo starts at 1), select Europe, calculate s = sqrt(u2+v2), select just s
-    cdo -selname,s_hub -expr,"s_hub=sqrt(U*U+V*V);" -sellonlatbox,-15,50,30,75 -sellevidx,32 /net/meso/climphys/cesm212/b.e212.BHISTcmip6.f09_g17.0900/archive/atm/hist/b.e212.BHISTcmip6.f09_g17.0900.cam.h3.${year}-01-01-00000.nc /net/xenon/climphys/lbloin/energy_boost/s_hub_${year}_mod.nc
+    cdo -selname,s_hub -expr,"s_hub=sqrt(U*U+V*V);" -sellonlatbox,-15,50,30,75 -sellevidx,32 /net/meso/climphys/cesm212/b.e212.BHISTcmip6.f09_g17.0900/archive/atm/hist/b.e212.BHISTcmip6.f09_g17.0900.cam.h3.${year}-01-01-00000.nc ../../output/s_hub_${year}_mod.nc
     
 done
 
-cdo -mergetime /net/xenon/climphys/lbloin/energy_boost/s_hub_*_mod.nc ../../output/hist_s_hub.nc
-rm /net/xenon/climphys/lbloin/energy_boost/s_hub_*_mod.nc
+cdo -mergetime ../../output/s_hub_*_mod.nc ../../output/hist_s_hub.nc
+rm ../../output/s_hub_*_mod.nc
 
 
 
