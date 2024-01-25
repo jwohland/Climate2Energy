0c6315c7b49094726ac3c5c2b879313b06d86291
Luna Bloin-Wibe <luna.bloinwibe@env.ethz.ch>
2024-01-25 17:33:41 +0100

    intermediate pv save

--- code/run_all.py
+++ code/run_all.py
@@ -42,6 +42,7 @@ print("Bias correction finished. Next: conversion to capacity factors")
 # Step 2: Calculate capacity factors
 ####################
 ds_CF_PV = calculate_PV(ds_corr_PV, params=None)
+ds_CF_PV.to_netcdf(f"../output/PV/PV_{year}.nc")
 ds_CF_wind = convert_winds(
     ds_corr_wind,
     "Wind_power_" + str(year) + ".nc",  # TODO: either remove completely or store intermediate PV output as well before computing country averages
