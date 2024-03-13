284995033ecfe046b6f91c56014ba31f54ae579d
jwohland <jan.wohland@env.ethz.ch>
2024-03-13 11:55:32 +0100

    Include density correction in wind energy conversion, i.e., make it turbine specific

--- code/run_all.py
+++ code/run_all.py
@@ -50,9 +50,11 @@ print("Bias correction finished. Next: conversion to capacity factors")
 ds_CF_PV = calculate_PV(ds_corr_PV, params=None)
 ds_CF_PV.to_netcdf(f"../output/PV/PV_{year}.nc")
 ds_CF_wind = convert_winds(
-    density_correct_winds(ds_corr_wind, ds_rho), # todo currently we only do it for the density corrected winds. Maybe should also do it with unmodified winds to be able to compare.
+    ds_corr_wind,
+    ds_rho,
     alpha,
     f"Wind_power_{str(year)}.nc",
+    density_correct=True,  # if set to False, no density correction is performed
 )  # this expects that ds has variable called s_hub with hub height winds
 print("Capacity factors computed. Next: country subsets and saving data")
 
