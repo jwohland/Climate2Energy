edf168288f404af4a25a65bde37eef3715a7bc73
jwohland <jan.wohland@env.ethz.ch>
2024-04-02 15:48:58 +0200

    This file should run them all.

---
+++ code/run_all.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+for bc_realization in A B C
+do
+    echo $bc_realization
+    for scenario in historical SSP370
+    do
+      echo $scenario
+      for realization in A B C
+      do
+        echo $realization
+        conda activate CESM2energy_dev
+        python compute_generation.py scenario realization bc_realization
+        conda deactivate
+        conda activate demand_ninja
+        python compute_demand.py scenario realization bc_realization
+        conda deactivate
+      done
+    done
+done
