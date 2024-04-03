66fa2edfba8d436acf9ab1e9938f497bd2f396c8
jwohland <jan.wohland@env.ethz.ch>
2024-04-03 17:46:42 +0200

    1) Align output folders with new structure 2) Align period with discussion, namely 1995-2014 (including) 3) Give loop variables meaningful names (year and month instead of i and j) 4) define as variable when stuff is used repeatedly, e.g., ../output/bias_correction 5) in s_hub calculation: I moved the sellonlatbox to the very beginning as this should reduce I/O considerably

--- code/preprocess/preprocess_temperature_ERA5.sh
+++ code/preprocess/preprocess_temperature_ERA5.sh
@@ -1,12 +1,14 @@
 # needs to be executed as preprocess/preprocess_temperature_ERA5
-for i in {1990..2010}
+output_path=../output/bias_correction
+
+for year in {1995..2014}
     do
     # calculate daily average, remap (bilinear) to CESM grid, select Europe, change name to temperature, convert to celsius
-    cdo -b F32 -addc,-273.15 -setattribute,temperature@units="°C" -chname,t2m,temperature -sellonlatbox,-15,50,30,75 -remapbil,/net/meso/climphys/flehner/observations/era5/day/CESM_atm_grid.txt /net/atmos/data/ERA5_deterministic/recent/0.25deg_lat-lon_1h/processed/regrid/era5_deterministic_recent.t2m.025deg.1h."${i}".nc ../output/tmp_"${i}"_temperature.nc
+    cdo -b F32 -addc,-273.15 -setattribute,temperature@units="°C" -chname,t2m,temperature -sellonlatbox,-15,50,30,75 -remapbil,../inputs/CESM_atm_grid.txt /net/atmos/data/ERA5_deterministic/recent/0.25deg_lat-lon_1h/processed/regrid/era5_deterministic_recent.t2m.025deg.1h."${year}".nc ${output_path}/tmp_"${year}"_temperature_ERA5.nc
 
 done
 
-cdo mergetime ../output/tmp_*_temperature.nc ../output/temperature_ERA5.nc
+cdo mergetime ${output_path}/tmp_*_temperature_ERA5.nc ${output_path}/Raw_ERA5_temperature.nc
 
-rm ../output/tmp_*_temperature.nc
+rm ${output_path}/tmp_*_temperature_ERA5.nc
 
