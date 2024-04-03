cebca1ab9ba9775caa23492a6190d631bef87641
jwohland <jan.wohland@env.ethz.ch>
2024-04-03 14:47:45 +0200

    Optimize print statements during computation

--- code/run_all.sh
+++ code/run_all.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 for bc_realization in A B C
 do
-    echo $bc_realization
     for scenario in historical SSP370
     do
-      echo $scenario
       for realization in A B C
       do
-        echo $realization
         conda activate CESM2energy
         python compute_generation.py ${scenario} ${realization} ${bc_realization}
         conda deactivate
