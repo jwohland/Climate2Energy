51d2a65627c8fbb20d2b406eba7b7f9019d664cf
jwohland <jan.wohland@env.ethz.ch>
2024-04-03 17:46:42 +0200

    1) Align output folders with new structure 2) Align period with discussion, namely 1995-2014 (including) 3) Give loop variables meaningful names (year and month instead of i and j) 4) define as variable when stuff is used repeatedly, e.g., ../output/bias_correction 5) in s_hub calculation: I moved the sellonlatbox to the very beginning as this should reduce I/O considerably

--- code/preprocess/preprocess_s_hub_ERA5.sh
+++ code/preprocess/preprocess_s_hub_ERA5.sh
@@ -1,25 +1,26 @@
 # needs to be executed as preprocess/preprocess_s_hub_ERA5
+output_path=../output/bias_correction
+input_path_ERA5=/net/atmos/data/era5_cds/original/100u/1hr
 
-for i in {1990..2010}
+for year in {1995..2014}
     do
-    for j in 01 02 03 04 05 06 07 08 09 10 11 12
+    for month in 01 02 03 04 05 06 07 08 09 10 11 12
         do
-        #monthly to yearly file, change name to U
-        cdo mergetime -chname,100u,U /net/atmos/data/era5_cds/original/100u/1hr/"${i}"/100u_1hr_era5_"${i}""${j}".nc ../output/u_"${i}""${j}".nc
-        #monthly to yearly file, change name to V
-        cdo mergetime -chname,100v,V /net/atmos/data/era5_cds/original/100v/1hr/"${i}"/100v_1hr_era5_"${i}""${j}".nc ../output/v_"${i}""${j}".nc
+        #select Europe, monthly to yearly, change name to U
+        cdo sellonlatbox,-15,50,30,75 -mergetime -chname,100u,U ${input_path_ERA5}/"${year}"/100u_1hr_era5_"${year}""${month}".nc ${output_path}/u_"${year}""${month}".nc
+        #same for V
+        cdo sellonlatbox,-15,50,30,75 -mergetime -chname,100v,V ${input_path_ERA5}/"${year}"/100v_1hr_era5_"${year}""${month}".nc ${output_path}/v_"${year}""${month}".nc
         # add U an V to same file
-        cdo merge ../output/u_"${i}""${j}".nc ../output/v_"${i}""${j}".nc ../output/tmp_"${i}""${j}"_s_hub_uv.nc
-        # calculate s = sqrt(u^2 + v^2), select only s, remap (conservative), calculate 6-hourly mean, select Europe TODO: insta values
-        cdo sellonlatbox,-15,50,30,75 -remapcon,/net/meso/climphys/flehner/observations/era5/day/CESM_atm_grid.txt -selname,s_hub -expr,"s_hub=sqrt(U*U+V*V);" ../output/tmp_"${i}""${j}"_s_hub_uv.nc ../output/tmp_"${i}""${j}"_s_hub.nc
-        
-        rm ../output/u_"${i}""${j}".nc
-        rm ../output/v_"${i}""${j}".nc
-        rm ../output/tmp_"${i}""${j}"_s_hub_uv.nc
+        cdo merge ${output_path}/u_"${year}""${month}".nc ${output_path}/v_"${year}""${month}".nc ${output_path}/tmp_"${year}""${month}"_s_hub_uv.nc
+        # calculate s = sqrt(u^2 + v^2), select only s, remap (conservative)
+        cdo remapcon,../inputs/CESM_atm_grid.txt -selname,s_hub -expr,"s_hub=sqrt(U*U+V*V);" ${output_path}/tmp_"${year}""${month}"_s_hub_uv.nc ${output_path}/tmp_"${year}""${month}"_s_hub.nc
+        # delete intermediate files
+        rm ${output_path}/u_"${year}""${month}".nc
+        rm ${output_path}/v_"${year}""${month}".nc
+        rm ${output_path}/tmp_"${year}""${month}"_s_hub_uv.nc
         done
-
 done
 # merge all years
-cdo mergetime ../output/tmp_*_s_hub.nc ../output/s_hub_ERA5.nc
+cdo mergetime ${output_path}/tmp_*_s_hub.nc ${output_path}/Raw_ERA5_s_hub.nc
 
-rm ../output/tmp_*_s_hub.nc
+rm ${output_path}/tmp_*_s_hub.nc
