f51e7371f172c2155633b61ae50b87e15f5b51d1
jwohland <jan.wohland@env.ethz.ch>
2024-04-04 08:07:10 +0200

    Split CDO commands into multi-line statement (with \) to increase readibility.
    To increase performance, choose European domain + sponge layer early before regridding. See notebook #35 that this leads to the same result.

--- code/preprocess/preprocess_global_horizontal_ERA5.sh
+++ code/preprocess/preprocess_global_horizontal_ERA5.sh
@@ -4,11 +4,23 @@ input_path_ERA5=/net/atmos/data/ERA5_deterministic/recent/0.25deg_lat-lon_1h/pro
 
 for year in {1995..2014}
     do
-    # remap (conservative) to CESM grid, select Europe, change name to global_horizontal, convert to W/m2 (with units)
-    cdo -b F64 -divc,3600 -setattribute,global_horizontal@units="W/m^2" -chname,ssrd,global_horizontal -sellonlatbox,-15,50,30,75 -remapcon,../inputs/CESM_atm_grid.txt ${input_path_ERA5}/era5_deterministic_recent.ssrd.025deg.1h."${year}".nc ${output_path}/tmp_"${year}"_global_horizontal_ERA5.nc
-
+    # The following cdo comment is split into multiple lines to increase legibility. Here is what happens per line
+    # 1) Unit conversion to W/m2
+    # 2) rename from ssrd to global_horizontal
+    # 3) Choose European region of interest
+    # 4) Remap conservatively to CESM2 grid
+    # 5) Choose region with 5 degrees sponge in all directions (just to speed up computation)
+    # 6) ERA5 input file
+    # 7) output file
+    cdo -b F64 -divc,3600 -setattribute,global_horizontal@units="W/m^2" \
+    -chname,ssrd,global_horizontal \
+    -sellonlatbox,-15,50,30,75 \
+    -remapcon,../inputs/CESM_atm_grid.txt \
+    -sellonlatbox,-20,55,25,80 \
+    ${input_path_ERA5}/era5_deterministic_recent.ssrd.025deg.1h."${year}".nc \
+    ${output_path}/tmp_"${year}"_global_horizontal_ERA5.nc
 done
-
+# merge all years
 cdo mergetime ${output_path}/tmp_*_global_horizontal_ERA5.nc ${output_path}/Raw_ERA5_global_horizontal.nc
-
+# clean up
 rm ${output_path}/tmp_*_global_horizontal_ERA5.nc
--- code/preprocess/preprocess_s_hub_ERA5.sh
+++ code/preprocess/preprocess_s_hub_ERA5.sh
@@ -6,14 +6,14 @@ for year in {1995..2014}
     do
     for month in 01 02 03 04 05 06 07 08 09 10 11 12
         do
-        #select Europe, monthly to yearly, change name to U
-        cdo sellonlatbox,-15,50,30,75 -mergetime -chname,100u,U ${input_path_ERA5}/"${year}"/100u_1hr_era5_"${year}""${month}".nc ${output_path}/u_"${year}""${month}".nc
+        #select Europe + 5 degrees sponge, monthly to yearly, change name to U
+        cdo sellonlatbox,-20,55,25,80 -mergetime -chname,100u,U ${input_path_ERA5}/"${year}"/100u_1hr_era5_"${year}""${month}".nc ${output_path}/u_"${year}""${month}".nc
         #same for V
-        cdo sellonlatbox,-15,50,30,75 -mergetime -chname,100v,V ${input_path_ERA5}/"${year}"/100v_1hr_era5_"${year}""${month}".nc ${output_path}/v_"${year}""${month}".nc
+        cdo sellonlatbox,-20,55,25,80 -mergetime -chname,100v,V ${input_path_ERA5}/"${year}"/100v_1hr_era5_"${year}""${month}".nc ${output_path}/v_"${year}""${month}".nc
         # add U an V to same file
         cdo merge ${output_path}/u_"${year}""${month}".nc ${output_path}/v_"${year}""${month}".nc ${output_path}/tmp_"${year}""${month}"_s_hub_uv.nc
-        # calculate s = sqrt(u^2 + v^2), select only s, remap (conservative)
-        cdo remapcon,../inputs/CESM_atm_grid.txt -selname,s_hub -expr,"s_hub=sqrt(U*U+V*V);" ${output_path}/tmp_"${year}""${month}"_s_hub_uv.nc ${output_path}/tmp_"${year}""${month}"_s_hub.nc
+        # calculate s = sqrt(u^2 + v^2), select only s, remap (bilinearly), select Europe
+        cdo sellonlatbox,-15,50,30,75 -remapbil,../inputs/CESM_atm_grid.txt -selname,s_hub -expr,"s_hub=sqrt(U*U+V*V);" ${output_path}/tmp_"${year}""${month}"_s_hub_uv.nc ${output_path}/tmp_"${year}""${month}"_s_hub.nc
         # delete intermediate files
         rm ${output_path}/u_"${year}""${month}".nc
         rm ${output_path}/v_"${year}""${month}".nc
@@ -22,5 +22,5 @@ for year in {1995..2014}
 done
 # merge all years
 cdo mergetime ${output_path}/tmp_*_s_hub.nc ${output_path}/Raw_ERA5_s_hub.nc
-
+# clean up
 rm ${output_path}/tmp_*_s_hub.nc
