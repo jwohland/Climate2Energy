aa44d26dddae5e675dc92dfd591b05068d5dbbde
fradmm <fdemarco@ethz.ch>
2024-04-10 14:40:23 +0200

    weighted_aggregation function updated: included inflow of reservoir inflow and pumped hydro

--- code/conversion_to_hydro.py
+++ code/conversion_to_hydro.py
@@ -126,31 +126,37 @@ def weighted_aggregation(ds_runoff,tech):
     country_list=df_jrc['country_code'].unique().tolist()
 
     # Create the dataset
-    ds_C_ror = xr.Dataset(
+    ds_C = xr.Dataset(
         data_vars=dict(
-            C_ror=(["lat", "lon", "country"],  np.zeros((len(lat),len(lon),len(country_list)))),
+            normalized_capacity=(["lat", "lon", "country"],  np.zeros((len(lat),len(lon),len(country_list)))),
         ),
         coords=dict(
             country=country_list,
             lat=lat,
             lon=lon
         ),
-        attrs=dict(description="Installed ror capacity (normalized per country)"),
+        attrs=dict(description="Installed hydro capacity (normalized per country)"),
     )
+
+    # Define the type of power plants to consider
+    if tech == "ror":
+        type_code = ["HROR"]
+    elif tech == "inflow":
+        type_code = ["HDAM","HPHS"]
 
     # Fill the dataset  
     for country_code in country_list:
-        C_ror = np.zeros((len(lat),len(lon)))
+        C = np.zeros((len(lat),len(lon)))
         for ii in range(len(lon)): 
             for jj in range(len(lat)):
-                C_ror[jj,ii] = df_jrc["installed_capacity_MW"][(df_jrc["type"]=='HROR') & (df_jrc["country_code"]==country_code) & (lon_edge[ii]<df_jrc["lon"]) & (df_jrc["lon"]<lon_edge[ii+1]) & (lat_edge[jj]<df_jrc["lat"]) & (df_jrc["lat"]<lat_edge[jj+1])].sum()
+                C[jj,ii] = df_jrc["installed_capacity_MW"][(df_jrc["type"].isin(type_code)) & (df_jrc["country_code"]==country_code) & (lon_edge[ii]<df_jrc["lon"]) & (df_jrc["lon"]<lon_edge[ii+1]) & (lat_edge[jj]<df_jrc["lat"]) & (df_jrc["lat"]<lat_edge[jj+1])].sum()
 
-        ds_C_ror.C_ror.loc[{'country':country_code}] = C_ror[:,:] / df_jrc["installed_capacity_MW"][(df_jrc["type"]=='HROR') & (df_jrc["country_code"]==country_code)].sum()
+        ds_C.normalized_capacity.loc[{'country':country_code}] = C[:,:] / df_jrc["installed_capacity_MW"][(df_jrc["type"].isin(type_code)) & (df_jrc["country_code"]==country_code)].sum()
         
     # Calculate the runoff per country
     ds_w = []
     for country in country_list:
-        ds = (ds_C_ror.C_ror.sel(country=country) * ds_runoff.runoff).sum(('lat','lon'))
+        ds = (ds_C.normalized_capacity.sel(country=country) * ds_runoff.runoff).sum(('lat','lon'))
         ds_w.append(ds)
     ds_w = xr.concat(ds_w, dim='country')
     ds_w = ds_w.to_dataset(name='runoff')
