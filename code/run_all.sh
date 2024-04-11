c3e0cf638b04e87dfe35bea16c48b0236a9d38f5
jwohland <jan.wohland@env.ethz.ch>
2024-04-11 10:19:18 +0200

    Add log files in run_all seperately for generation and demand

--- code/run_all.sh
+++ code/run_all.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+mkdir -p ../logs
 for bc_realization in A B C
 do
     for scenario in historical SSP370
@@ -6,10 +7,10 @@ do
       for realization in A B C
       do
         conda activate CESM2energy
-        python compute_generation.py ${scenario} ${realization} ${bc_realization}
+        python compute_generation.py ${scenario} ${realization} ${bc_realization} > ../logs/compute_generation_log_$(date +%Y_%m_%d_%H).txt
         conda deactivate
         conda activate demand_ninja
-        python compute_demand.py ${scenario} ${realization} ${bc_realization}
+        python compute_demand.py ${scenario} ${realization} ${bc_realization} > ../logs/compute_demand_log_$(date +%Y_%m_%d_%H).txt
         conda deactivate
       done
     done
