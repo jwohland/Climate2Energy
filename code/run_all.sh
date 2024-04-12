49b47e1fb5c7ad4a14dc49cdad490c0e0faa02ea
jwohland <jan.wohland@env.ethz.ch>
2024-04-12 12:55:24 +0200

    Update run_all

--- code/run_all.sh
+++ code/run_all.sh
@@ -9,15 +9,16 @@ do
     do
       for realization in A B C
       do
+        identifier=${scenario}_${realization}_${bc_realization}
         conda activate CESM2energy
-        python compute_bias_correction.py ${scenario} ${realization} ${bc_realization}
-        python compute_generation.py ${scenario} ${realization} ${bc_realization} Wind > ../logs/compute_generation_wind_log_$(date +%Y_%m_%d_%H).txt
+        python compute_bias_correction.py ${scenario} ${realization} ${bc_realization} | tee ../logs/compute_bias_correction_log_$(date +%Y_%m_%d_%H)_${identifier}.txt
+        python compute_generation.py ${scenario} ${realization} ${bc_realization} Wind | tee ../logs/compute_generation_wind_log_$(date +%Y_%m_%d_%H)_${identifier}.txt
         # Optional also run without density correction
         # python compute_generation.py ${scenario} ${realization} ${bc_realization} Wind False  > ../logs/compute_generation_wind_log_$(date +%Y_%m_%d_%H).txt
-        python compute_generation.py ${scenario} ${realization} ${bc_realization} PV > ../logs/compute_generation_PV_log_$(date +%Y_%m_%d_%H).txt
+        python compute_generation.py ${scenario} ${realization} ${bc_realization} PV | tee ../logs/compute_generation_PV_log_$(date +%Y_%m_%d_%H)_${identifier}.txt
         conda deactivate
         conda activate demand_ninja
-        python compute_demand.py ${scenario} ${realization} ${bc_realization} > ../logs/compute_demand_log_$(date +%Y_%m_%d_%H).txt
+        python compute_demand.py ${scenario} ${realization} ${bc_realization} | tee ../logs/compute_demand_log_$(date +%Y_%m_%d_%H)_${identifier}.txt
         conda deactivate
       done
     done
