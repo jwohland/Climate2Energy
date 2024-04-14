91ef23609bebb037f7c9cf6008b62c50de53e077
Luna Bloin-Wibe <luna.bloinwibe@env.ethz.ch>
2024-04-14 17:00:11 +0200

    resample era5 to daily in preproc

--- code/preprocess/preprocess_runoff_ERA5.sh
+++ code/preprocess/preprocess_runoff_ERA5.sh
@@ -3,7 +3,7 @@
 for i in {1995..2015}
     do
     # calculate daily average (era5 runoff is in units of m, no time unit), remap (conservative) to CESM grid, select Europe, change to fit mm/d
-    cdo -b F32 -setattribute,runoff@units="mm/d" -mulc,1000 -selname,runoff -chname,ro,runoff -sellonlatbox,-15,50,30,75 -remapcon,/net/meso/climphys/flehner/observations/era5/day/CESM_atm_grid.txt /net/atmos/data/ERA5_deterministic/recent/0.25deg_lat-lon_1h/original/era5_deterministic_recent.ro.025deg.1h."${i}".nc ../output/tmp_"${i}"_runoff.nc
+    cdo -b F32 -setattribute,runoff@units="mm/d" -mulc,1000 -sellonlatbox,-15,50,30,75 -selname,runoff -chname,ro,runoff -daysum -remapcon,/net/meso/climphys/flehner/observations/era5/day/CESM_atm_grid.txt /net/atmos/data/ERA5_deterministic/recent/0.25deg_lat-lon_1h/original/era5_deterministic_recent.ro.025deg.1h."${i}".nc ../output/tmp_"${i}"_runoff.nc
 
 done
 
