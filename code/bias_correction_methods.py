f9288926448e52268ac7e00f13ad135fa72eae36
Luna Bloin-Wibe <luna.bloinwibe@env.ethz.ch>
2024-07-18 16:48:17 +0200

    simplified regridding by coarsening x10 for hydro, and added bias_corr method for hydro

--- code/bias_correction_methods.py
+++ code/bias_correction_methods.py
@@ -84,11 +84,6 @@ def bias_correct_dataset(ds, var, bc_realization, method="basic_quantile"):
     if glob.glob(mod_file) == []:
         print(f"missing historical model file for {var}")
         subprocess.run(["bash", f"preprocess/preprocess_{var}_model_hist.sh"])
-        if var == "discharge":
-            subprocess.run(["bash", f"preprocess/preprocess_runoff_model_hist.sh"])
-            ds_discharge = xr.open_dataset(f'../output/hist_discharge_monthly.nc')
-            ds_runoff = xr.open_dataset(f"../output/hist_runoff.nc")
-            downscale(ds_discharge,ds_runoff,"hist_discharge") #downscale from monthly to daily discharge values using daily runoff 
     # open reference and model data
     reference = zero_mean_longitudes(xr.open_dataset(ref_file))
     model = zero_mean_longitudes(xr.open_dataset(mod_file))
