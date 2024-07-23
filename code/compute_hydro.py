996ddc5f93b6d288538b8338326bcff8d88492dc
Luna Bloin-Wibe <luna.bloinwibe@env.ethz.ch>
2024-07-23 14:30:32 +0200

    save intermediate bced discharge year by year

--- code/compute_hydro.py
+++ code/compute_hydro.py
@@ -33,7 +33,9 @@ if __name__ == "__main__":
                                         era5_discharge_full.sel(time=slice("1995","2014")).discharge.load(), 
                                         discharge_full_for_bc.discharge
                                        ).to_dataset(name="discharge").convert_calendar("proleptic_gregorian") # to get numpy datetime (necessary for weekly resampling)
-    
+    # save bias corrected discharge, year for year
+    for year in get_time_range(scenario):
+        discharge_full.sel(time="year").to_netcdf(f"../output/bias_correction/{bc_realization}/{scenario}/{realization}/atmospheric_variables/bced_CESM2_discharge_{year}.nc")
     # aggregation
     for tech in technologies:
         print(f"Aggregate CESM2 and ERA5 for tech {tech}")
