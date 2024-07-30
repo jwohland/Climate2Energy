c2e6488170a300a97c08b995c6aef6956b36e548
Jan Wohland <jan.wohland@its.uio.no>
2024-07-30 14:26:13 +0200

    Changing variable names to more meaningful ones.
    Loop over countries not needed because is broadcasted, so can be removed.

--- code/conversion_to_hydro.py
+++ code/conversion_to_hydro.py
@@ -365,19 +365,19 @@ def create_entsoe_inflow():
     for country in tqdm(country_list, desc="Reading filling levels per country"):
         filename = f"../inputs/entsoe_historic_inflow/{country}/Water Reservoirs and Hydro Storage Plants_201412290000-202412300000.csv"
         df_V = pd.read_csv(filename)
-        V = []
+        V_list = []
         for year in range(year_0, year_N + 1):
             mask = df_V.columns.str.contains(str(year))
-            V.extend(
+            V_list.extend(
                 df_V.loc[: len(df_time[df_time.year == year]) - 1, mask]
                 .interpolate()
                 .copy()
                 .values.flatten()
                 / 1000
             )
-        ds_old["V"].loc[{"country": country}] = xr.DataArray(V, dims=("time"))
+        ds_old["V"].loc[{"country": country}] = xr.DataArray(V_list, dims=("time"))
 
-    ## CALCULATE DELTA V ##
+    ## CALCULATE CHANGE IN FILLING LEVEL DELTA V ##
     ds_old["delta_V"] = ds_old["V"].diff("time")
     ds_old["delta_V"] = ds_old["delta_V"].shift(time=-1)
 
@@ -388,20 +388,20 @@ def create_entsoe_inflow():
         df_gen = pd.DataFrame(columns=["time", "gen_GWh"])
         for year in range(year_0, year_N + 1):
             filename = f"../inputs/entsoe_historic_inflow/{country}/Actual Generation per Production Type_{str(year)}01010000-{str(year+1)}01010000.csv"
-            df_gen_y = pd.read_csv(filename)
-            df_gen_y = df_gen_y.interpolate()
-            df_gen_y["time"] = df_gen_y["MTU"].apply(
+            df_gen_year = pd.read_csv(filename)
+            df_gen_year = df_gen_year.interpolate()
+            df_gen_year["time"] = df_gen_year["MTU"].apply(
                 lambda x: dt.datetime.strptime(x[:15], date_format)
             )
-            df_gen_y["time_delta"] = (
-                df_gen_y.time.shift(-1) - df_gen_y.time
+            df_gen_year["time_delta"] = (
+                df_gen_year.time.shift(-1) - df_gen_year.time
             ).dt.total_seconds() / 3600
-            df_gen_y["gen_GWh"] = (
-                df_gen_y["Hydro Water Reservoir  - Actual Aggregated [MW]"]
-                * df_gen_y["time_delta"]
+            df_gen_year["gen_GWh"] = (
+                df_gen_year["Hydro Water Reservoir  - Actual Aggregated [MW]"]
+                * df_gen_year["time_delta"]
                 / 1000
             )
-            df_gen = pd.concat([df_gen, df_gen_y[["time", "gen_GWh"]]], axis=0)
+            df_gen = pd.concat([df_gen, df_gen_year[["time", "gen_GWh"]]], axis=0)
         sum_gen = []
         for date in df_time.date:
             start_date = date
@@ -415,11 +415,10 @@ def create_entsoe_inflow():
 
     ## CALCULATE INFLOW ##
     eff = 0.9**0.5
-    for country in country_list:
-        ds_old["inflow_GWh"].loc[{"country": country}] = (
-            ds_old["gen"].loc[{"country": country}] / eff
-            + ds_old["delta_V"].loc[{"country": country}]
-        )
+    ds_old["inflow_GWh"] = (
+        ds_old["gen"] / eff  # energy that left the reservoir to generate electricity
+        + ds_old["delta_V"]  # energy that entered via water flowing into reservoir
+    )
 
     ## ADJUST INFLOW LEVELS ##
 
