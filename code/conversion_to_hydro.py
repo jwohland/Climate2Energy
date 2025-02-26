136196620b6b9596c86ee21dc2c92b7e35e70bdf
Luna Bloin-Wibe <luna.bloinwibe@env.ethz.ch>
2025-02-26 13:57:38 +0100

    fixed bugs as described in issue 65

--- code/conversion_to_hydro.py
+++ code/conversion_to_hydro.py
@@ -63,12 +63,12 @@ def downscale(ds_discharge, ds_runoff, file_name):
     ## Expand monthly discharge to daily discharge
     ds_discharge_daily = (
         ds_discharge.resample(time="1D")
-        .ffill()
+        .bfill()
         .rename({"discharge": "discharge_expanded"})
     )
-    ds_discharge_daily["time"] = ds_discharge_daily.time + dt.timedelta(
-        days=-31
-    )  # shift one month back
+    # ds_discharge_daily["time"] = ds_discharge_daily.time + dt.timedelta(
+    #     days=-31
+    # )  # shift one month back
     ## Match time coordinates of the two datasets: discharge_daily and runoff_mean
     ds_runoff_mean = ds_runoff_mean.sel(time=ds_discharge_daily.time)
     ## Expand runoff_mean, to include latitude and longitude
@@ -140,8 +140,12 @@ def create_discharge(scenario, realization):
     # add the last file. example: 2100-01-01 has info about 2099-12-31, so needs to be added
     year = year + 1  # year after last year in time range
     month = "01"
+    if period == "HIST":
+        period_here = "SSP370" # 2015 is already in future, but its so close that SSP choice doesnt matter
+    else:
+        period_here = period
     files_dis.append(
-        f"{path}rof/hist/b.e212.B{period}cmip6.f09_g17.{file_real}.mosart.h0.{year}-{month}.nc"
+        f"/net/meso/climphys/cesm212/b.e212.B{period_here}cmip6.f09_g17.{file_real}/archive/rof/hist/b.e212.B{period_here}cmip6.f09_g17.{file_real}.mosart.h0.{year}-{month}.nc"
     )
     # get monthly discharge
     discharge = xr.open_mfdataset(
