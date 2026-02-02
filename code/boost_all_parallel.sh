256892cc77be14ca47362050175dbbe18930f718
Luna Bloin-Wibe <luna.bloinwibe@env.ethz.ch>
2026-02-02 13:08:39 +0100

    made parallelization ne equal to number of boosted members

--- code/boost_all_parallel.sh
+++ code/boost_all_parallel.sh
@@ -5,17 +5,18 @@ set -o pipefail
 # === INPUTS ===
 # ==============
 
-realization="B"
-scenario="historical"
-year="1996"
+realization="A"
+scenario="SSP370"
+year="2080"
 dates=(
-  "1996-01-20"
-  "1996-01-23"
-  "1996-01-26"
-  "1996-01-29"
-  "1996-02-01"
+  "2080-02-14"
+  "2080-02-16"
+  "2080-02-18"
+  "2080-12-01"
+  "2080-12-03"
+  "2080-12-05"
 )
-
+members=10
 # get the correct realization number for file naming convention
 conda activate iacpy3_2024
 nb_realization=$(python3 - <<EOF
@@ -26,12 +27,12 @@ EOF
 conda deactivate
 
 # How many parallel jobs at once?
-PARALLEL_JOBS=50	
+PARALLEL_JOBS=${members}	
 
 # run boost_one_date_one_member for different dates and members
 for date in "${dates[@]}"; do
 	echo "${realization} ${nb_realization} ${scenario} ${year} ${date}"
-	parallel -j ${PARALLEL_JOBS} bash -l boost_one_member_one_date.sh ${realization} ${nb_realization} ${scenario} ${year} ${date} ::: {1..50}
+	parallel -j ${PARALLEL_JOBS} bash -l boost_one_member_one_date.sh ${realization} ${nb_realization} ${scenario} ${year} ${date} ::: $(seq 1 "${members}")
 
 done
 
--- code/boost_one_member_one_date.sh
+++ code/boost_one_member_one_date.sh
@@ -26,12 +26,14 @@ input_path_discharge="B${scenario}cmip6.100${nb_realization}.${date}.ens${ens}"
 input_info="boost_${date}_ens${ens}"
 output_path="/net/xenon/climphys/lbloin/CESM2energy/output/boost/${realization}/"
 input_path="${output_path}/atmospheric_variables/atmospheric_variables_${date}_ens${ens}"
+
 # compute 1st part of translation in CESM2energy env
-#conda activate CESM2energy
+conda activate CESM2energy
 python compute_bias_correction.py ${scenario} ${realization} ${bc_realization} ${input_path} ${input_info} ${year} ${output_path} True | tee ../logs/compute_bias_correction_log_$(date +%Y_%m_%d_%H)_${input_info}.txt
 python compute_generation.py ${scenario} ${realization} ${bc_realization} Wind ${input_info} True ${output_path} | tee ../logs/compute_generation_wind_log_$(date +%Y_%m_%d_%H)_${input_info}.txt
 python compute_generation.py ${scenario} ${realization} ${bc_realization} PV ${input_info} True ${output_path} | tee ../logs/compute_generation_PV_log_$(date +%Y_%m_%d_%H)_${input_info}.txt
 conda deactivate
+
 # compute 2nd part of translation in demand_ninja env
 conda activate demand_ninja
 python compute_demand.py ${scenario} ${realization} ${bc_realization} ${input_info} ${output_path} | tee ../logs/compute_demand_log_$(date +%Y_%m_%d_%H)_${identifier}${input_info}.txt
